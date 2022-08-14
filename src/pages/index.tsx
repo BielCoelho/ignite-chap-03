13c6b0dd45327d4fd630633f2da83a4b4630e709
Gabriel Coelho <gabrielcuei98@gmail.com>
2022-08-14 23:12:08 +0100

    Header done & placeholder for dynamic signIn button created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
-import styles from '../styles/home.module.scss';
+import Head from 'next/head'
 
 export default function Home() {
   return (
-   <h1 className={styles.title}>Hello World</h1>
+    <>
+    <Head>
+      <title>ig.news | Home</title>
+   </Head>
+   
+   
+   <main>
+    <section>
+
+    </section>
+   </main>
+   <img src="/images/avatar.svg" alt="Girl coding" />
+    </>
   )
 }
