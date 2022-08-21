183dad2ba5bc5b29024fc7c50f75a3d54c3886bc
Gabriel Coelho <gabrielcuei98@gmail.com>
2022-08-21 19:11:03 +0100

    bugged(console not logging the buffer from webhook)

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,5 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from "stream";
+import Stripe from "stripe";
+import { stripe } from "../../services/stripe";
 
 async function buffer(readable: Readable) {
     const chunks = [];
@@ -19,8 +21,32 @@ export const config = {
     }
 }
 
+const relevantEvents = new Set([
+    'checkout.session.completed',
+])
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === 'POST') {
     const buf = await buffer(req)
+    const secret = req.headers['stripe-signature']
+
+    let event: Stripe.Event;
+
+    try {
+        event =  stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET)
+    } catch (err) {
+        return res.status(400).send(`Webhook Error: ${err.message}`)
+    }
+
+    const type = event.type
+
+    if (relevantEvents.has(type)) {
+        console.log(`Evento recebido: ${event}`)
+    }
 
     res.status(200).json({ ok: true})
+    } else {
+        res.setHeader('Allow', 'POST')
+        res.status(405).end('Method not allowed')
+    }
 }
